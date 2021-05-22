a902a703dd41f09cc1f95331cb63501e54b3ab92
higuchimakoto <makoto-higuchi@tapp-co.jp>
2021-05-22 18:43:21 +0900

    グローバルコンテキストの追加

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -5,6 +5,7 @@ import { Center, Wrap, WrapItem } from '@chakra-ui/layout'
 import { Spinner } from '@chakra-ui/spinner'
 import React, { memo, useCallback, useEffect, VFC } from 'react'
 import { useAllUsers } from '../../hooks/useAllUsers'
+import { useLoginUser } from '../../hooks/useLoginUser'
 import { UserCard } from './organisms/user/UserCard'
 import { UserDetailModal } from './organisms/user/UserDetailModal'
 import { useSelectUser } from './useSelectUser'
@@ -13,6 +14,9 @@ export const UserManagement: VFC = memo(() => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { getUsers, users, loading } = useAllUsers()
     const { selectedUser, onSelectUser } = useSelectUser()
+    const { loginUser } = useLoginUser()
+    console.log(loginUser);
+
 
     // 画面表示時に１回だけ処理をする場合
     useEffect(() => {
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,6 +3,7 @@ import axios from "axios"
 import { useCallback, useState } from "react"
 import { useHistory } from "react-router-dom"
 import { User } from "../types/api/user"
+import { useLoginUser } from "./useLoginUser"
 import { useMessage } from "./useMessage"
 
 
@@ -10,12 +11,14 @@ export const useAuth = () => {
   const history = useHistory()
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false)
+  const { setLoginUser } = useLoginUser()
 
   const login = useCallback((id: string) => {
     setLoading(true)
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          setLoginUser(res.data)
           showMessage({ title: 'ログインしました。', status: 'success' })
           history.push('/home')
         } else {
